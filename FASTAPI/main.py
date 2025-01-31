7d71fff9377d591c0d3bd29b71c61486cc44cdc6
CarlosVelazquezG <carlos.velazquez1235@outlook.com>
2025-01-30 18:54:20 -0600

    Practica Swagger UI
    Práctica Swagger

--- FASTAPI/main.py
+++ FASTAPI/main.py
@@ -1,8 +1,65 @@
 
 from fastapi import FastAPI
+from typing import Optional
 
-app = FastAPI()
+app = FastAPI(
+    title = 'Mi primer API 196', 
+    description = 'Carlos Velázquez Govea',
+    version = '1.0.1'
+)
 
-@app.get('/')
+usuarios = [
+    {"id":1,"nombre":"alondra", "edad":22},
+    {"id":2,"nombre":"carlos", "edad":22},
+    {"id":3,"nombre":"antonio", "edad":23},
+    {"id":4,"nombre":"danna", "edad":21}
+]
+
+@app.get('/',tags=['Inicio'])
 def main():
     return {'Hola FastAPI':'CarlosVelazquez'}
+
+@app.get('/promedio', tags=['Mi calificación TAI'])
+def promedio():
+    promedio = 20.20
+    return {promedio}
+
+#Endpoint Parámetro Obligatorio
+@app.get('/usuario/{id}',tags=['Parámetro obligatorio'])
+def consultaUsuario(id:int):
+    #ConnectDB
+    #Query
+    return{"Se encontró el usuario": id}
+
+#Endoint Parámetro Opcional
+@app.get('/usuario/',tags=['Parámetro opcional'])
+def consultaUsuario2(id:Optional[int]= None):
+    if id is not None:
+        for usuario in usuarios:
+            if usuario["id"]== id:
+                return {"mensaje":"Usuario encontrado","usuario":usuario}
+        return{"mensaje":f"No se encontró el id: {id}"}
+    else:
+        return {"mensaje":"No se proporcionó un Id"}
+    
+#endpoint con varios parametro opcionales
+@app.get("/usuarios/", tags=["3 parámetros opcionales"])
+async def consulta_usuarios(
+    usuario_id: Optional[int] = None,
+    nombre: Optional[str] = None,
+    edad: Optional[int] = None
+):
+    resultados = []
+
+    for usuario in usuarios:
+        if (
+            (usuario_id is None or usuario["id"] == usuario_id) and
+            (nombre is None or usuario["nombre"].lower() == nombre.lower()) and
+            (edad is None or usuario["edad"] == edad)
+        ):
+            resultados.append(usuario)
+
+    if resultados:
+        return {"usuarios_encontrados": resultados}
+    else:
+        return {"mensaje": "No se encontraron usuarios que coincidan con los parámetros proporcionados."}
